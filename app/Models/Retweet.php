ea19346d761585cafb4d376ea8ef783868f6c2bb
jeneya <jeyacabildo@gmail.com>
2025-11-24 23:16:09 +0800

    Update Retweet.php

--- app/Models/Retweet.php
+++ app/Models/Retweet.php
@@ -22,7 +22,7 @@ class Retweet extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class, 'user_id', 'user_id');
+        return $this->belongsTo(User::class, 'user_id', 'id');
     }
 
     public function tweet()
