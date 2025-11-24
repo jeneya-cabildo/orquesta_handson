4a65d0c978e1092c0fe28750397040d003c1686a
jeneya <jeyacabildo@gmail.com>
2025-11-24 23:10:45 +0800

    Update show.blade.php

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -64,7 +64,13 @@
                                 <svg class="w-3 h-3 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
                                 </svg>
-                                {{ $tweet->likes_count ?? $tweet->likes->count() ?? 0 }}
+                                @if(isset($tweet->likes_count))
+                                    {{ $tweet->likes_count }}
+                                @elseif(isset($tweet->likes) && is_object($tweet->likes) && method_exists($tweet->likes, 'count'))
+                                    {{ $tweet->likes->count() }}
+                                @else
+                                    0
+                                @endif
                             </span>
                         </div>
                     </div>
